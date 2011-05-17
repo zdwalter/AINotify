0a2f8301a924035b3c61df71bcda050252a3ac40
Walter Zheng <zdwalter@gmail.com>
2011-05-17 18:41:06 +0800

    gps

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 joCache.set("map", function() {
     var path = "http://maps.google.com/maps/api/staticmap?center=";
     var card = new joCard([
-        new joView("<div id='map_canvas'></div>"),
+        new joView("<div id='map_canvas' style='height: 320px; width: 320px;'></div>"),
         new joLabel("Where you are now")
         ]);
 
@@ -11,17 +11,12 @@ joCache.set("map", function() {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            var myOptions = {
-                zoom: 6,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-            App.map = new google.maps.Map(joDOM.get("map_canvas"), myOptions);
-            App.initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            App.infowindow = new google.maps.InfoWindow();
+            map_init();
+            var initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
             //contentString = "Location found using W3C standard";
-            App.map.setCenter(App.initialLocation);
+            App.map.setCenter(initialLocation);
             //infowindow.setContent(contentString);
-            App.infowindow.setPosition(App.initialLocation);
+            App.infowindow.setPosition(initialLocation);
             App.infowindow.open(App.map);
             //var location = "" + position.coords.latitude + "," + position.coords.longitude;
             ////path += location + "&zoom=" + zoom;
@@ -32,9 +27,27 @@ joCache.set("map", function() {
             //joDOM.get('static_map').src = path;
             //console.log('got location:' + path);
         }, function () {
-        joDOM.get('map_canvas').innerHTML = "fail to load location";
+            map_init();
+            var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+            App.initialLocation = newyork;
+            App.map.setCenter(App.initialLocation);
+            App.infowindow.setPosition(App.initialLocation);
+            App.infowindow.open(App.map);
+            //joDOM.get('map_canvas').innerHTML = "fail to load location";
         });
     }
 
     return card;
 });
+
+function map_init() {
+var myOptions = {
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+App.map = new google.maps.Map(joDOM.get("map_canvas"), myOptions);
+
+App.infowindow = new google.maps.InfoWindow();
+};
+           
+
