d0d0558fba1f51d3d6fd69e8e729e199511dd7b0
Walter Zheng <zdwalter@gmail.com>
2011-05-18 08:14:36 +0800

    gps

--- js/map.js
+++ js/map.js
@@ -1,6 +1,5 @@
 
 joCache.set("map", function() {
-    var path = "http://maps.google.com/maps/api/staticmap?center=";
     var card = new joCard([
         new joView("<div id='map_canvas' style='height: 320px; width: 320px;'></div>"),
         new joLabel("Where you are now")
@@ -10,7 +9,8 @@ joCache.set("map", function() {
     //TODO: load map
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
+        var path = "http://maps.google.com/maps/api/staticmap?center=";
+        navigator.geolocation.getCurrentPosition(function success(position) {
             map_init();
             var initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
             //contentString = "Location found using W3C standard";
@@ -18,15 +18,7 @@ joCache.set("map", function() {
             //infowindow.setContent(contentString);
             App.infowindow.setPosition(initialLocation);
             App.infowindow.open(App.map);
-            //var location = "" + position.coords.latitude + "," + position.coords.longitude;
-            ////path += location + "&zoom=" + zoom;
-            //path += "&size=320x320&markers=color:red|label:P|";
-            ////path += "&size=250x250&maptype=" + map + "&markers=color:       red|label:P|";
-            //path += location + "&sensor=false";
-
-            //joDOM.get('static_map').src = path;
-            //console.log('got location:' + path);
-        }, function () {
+        }, function error() {
             map_init();
             var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
             App.initialLocation = newyork;
